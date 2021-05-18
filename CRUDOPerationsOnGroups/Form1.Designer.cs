31cbd168c60fbf7cb7f43c76ba3ffe848aed4300
Shrushti Somshette <shrushti.somshette@nitorinfotech.net>
2021-05-18 16:30:06 +0530

    first

---
+++ CRUDOPerationsOnGroups/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CRUDOPerationsOnGroups
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Retriving All usersfromList";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Retrive Specific Property";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(34, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 85);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Retriving All USers from All groups";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(294, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 85);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Adding User in group";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(565, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(168, 65);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Update User Info in GROUP";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(565, 156);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(168, 85);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Remove user from list";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(34, 42);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(177, 70);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Create Group ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(305, 42);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(152, 70);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Update Group Name";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(565, 42);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(168, 70);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Delete Group";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 430);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ CRUDOPerationsOnGroups/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SharePoint.Client;
+
+namespace CRUDOPerationsOnGroups
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Method To get All users from the perticular group
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("URL"))
+            {
+                Web web = ctx.Web;
+                GroupCollection groups = web.SiteGroups;
+                Group group = groups.GetByName("HOD");
+                UserCollection users = group.Users;
+                ctx.Load(users);
+                ctx.ExecuteQuery();
+                foreach(User user in users)
+                {
+                    MessageBox.Show(user.Email+ "  " + user.LoginName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method To retrive specific properties of users from the groups
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("URL"))
+            {
+                Web web = ctx.Web;
+                GroupCollection groups = web.SiteGroups;
+                Group group = groups.GetByName("HOD");
+                UserCollection users = group.Users;
+                ctx.Load(users, pusers => pusers.Include(user => user.Title, user => user.LoginName, user => user.Email));
+                ctx.ExecuteQuery();
+                foreach (User ouser in users)
+                {
+                    MessageBox.Show(ouser.Title + " " +ouser.LoginName + " " + ouser.Email);
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to retrive all the users from all the groups present on the site collection
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("URL"))
+            {
+                Web web = ctx.Web;
+                GroupCollection collgroups = web.SiteGroups;
+                ctx.Load(collgroups);
+
+                ctx.Load(collgroups,
+                    groups => groups.Include(
+                        group => group.Users));
+                ctx.ExecuteQuery();
+                foreach (Group oGroup in collgroups)
+                {
+                    UserCollection collUser = oGroup.Users;
+
+                    foreach (User oUser in collUser)
+                    {
+                        MessageBox.Show(oGroup.Id+" "+oGroup.Title + " " + oUser.Title + " " + oUser.LoginName);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Method to add USER in the perticular list 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("URL"))
+            {
+                Web web = ctx.Web;
+                GroupCollection groups = web.SiteGroups;
+                Group group = groups.GetByName("HOD");
+                UserCreationInformation userCreationInformation = new UserCreationInformation();
+                userCreationInformation.LoginName = @"domain\hrmsemployee";
+                userCreationInformation.Email = "ssr@gmail.com";
+                User user = group.Users.Add(userCreationInformation);
+                ctx.ExecuteQuery();
+            }
+            MessageBox.Show("User Added to the group");
+        }
+        /// <summary>
+        /// Method to update the information of user present in the group
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("URL"))
+            {
+                Web web = ctx.Web;
+                GroupCollection groups = web.SiteGroups;
+                Group group = groups.GetByName("HOD");
+                UserCollection users = group.Users;
+                User user = users.GetByLoginName(@"Domain\hrmsemployee");
+                ctx.Load(user);
+                user.Email = "rudra@gmail.com";
+                user.Update();
+                ctx.ExecuteQuery();
+            }
+            MessageBox.Show("User Updated Sucessfully");
+        }
+
+        /// <summary>
+        /// Method to Delete User from the Group
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("http://hr.zubaircorp.com/"))
+            {
+                Web web = ctx.Web;
+                GroupCollection groups = web.SiteGroups;
+                Group group = groups.GetByName("HOD");
+                UserCollection users = group.Users;
+                group.Users.RemoveByLoginName(@"zubaircorp\hrmsemployee");
+                ctx.ExecuteQuery();
+            }
+            MessageBox.Show("User Removed Sucessfully");
+        }
+        /// <summary>
+        /// Method to create new group in sharepoint site
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button7_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("http://hr.zubaircorp.com/"))
+            {
+                Web web = ctx.Web;
+                GroupCreationInformation groupCreation = new GroupCreationInformation();
+                groupCreation.Title = "Testing123";
+                Group group = web.SiteGroups.Add(groupCreation);
+                ctx.ExecuteQuery();
+            }
+            MessageBox.Show("Group Created Successfully");
+        }
+        /// <summary>
+        /// Method to Update the group name 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button8_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("http://hr.zubaircorp.com/"))
+            {
+                Web web = ctx.Web;
+                Group group = web.SiteGroups.GetByName("Testing123");
+                group.Title = "Testing34";
+                group.Update();
+                ctx.ExecuteQuery();
+            }
+            MessageBox.Show("Group Name UPdated Successfully");
+        }
+        /// <summary>
+        /// Remove/ Delete group from site
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button9_Click(object sender, EventArgs e)
+        {
+            using (ClientContext ctx = new ClientContext("http://hr.zubaircorp.com/"))
+            {
+                Web web = ctx.Web;
+                Group group = web.SiteGroups.GetByName("Testing34");
+                web.SiteGroups.Remove(group);
+                ctx.ExecuteQuery();
+            }
+            MessageBox.Show("Group Deleted Successfully");
+        }
+    }
+}
